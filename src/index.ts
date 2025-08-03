0da2dc0a2181893f6ed1bf8b61717adb755eeca1
forgere <forgere123zw@gmail.com>
2025-08-03 15:35:15 +0800

    remove useless code

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,2 @@
 export type { InstancedMeshPoolProps } from "./components/InstanceMeshPool";
 export { InstancedMeshPool } from "./components/InstanceMeshPool";
-export type { MeshPoolProps } from "./components/MeshPool";
-export { MeshPool } from "./components/MeshPool";
